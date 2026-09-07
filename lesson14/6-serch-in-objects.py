71f418fc00244a6f74a9d63e0c79e7fd7701e65d
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:01:12 +0330

    Delete lesson14/6-serch-in-objects.py

--- lesson14/6-serch-in-objects.py
+++
@@ -1,8 +0,0 @@
-favorite_language = {
-    'benyamin': 'JavaScript',
-    'mehrsam': 'Python',
-    'hesam': "C",
-    'taha': 'JavaScript'
-}
-language = favorite_language['mehrsam'].title()
-print(f"Sina's favorite language is {language}")
