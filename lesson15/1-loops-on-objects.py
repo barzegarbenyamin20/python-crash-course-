89cd8a6a8041c91eda67ef3352a7c3dfc5f0c6ae
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:02:13 +0330

    Delete lesson15/1-loops-on-objects.py

--- lesson15/1-loops-on-objects.py
+++
@@ -1,8 +0,0 @@
-user_0 = {
-    'username': 'sina.sasani.497',
-    'first': 'benyamin',
-    'last': 'benyamin',
-}
-for key, value in user_0.items():
-    print(f'\nKeys: {key}')
-    print(f'Values: {value}')
