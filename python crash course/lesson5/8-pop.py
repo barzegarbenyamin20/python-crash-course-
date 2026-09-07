6e0d262458cdbfd4bd96018e99c72a160141b5f4
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:19:49 +0330

    Delete python crash course/lesson5/8-pop.py

--- python crash course/lesson5/8-pop.py
+++
@@ -1,6 +0,0 @@
-cars = ["bmw","bugati","scania","volvo"]
-print(cars)
-last_owend_cars = cars.pop(1)
-print(f"My favorite car is a {last_owend_cars.upper()}")
-first_owend_cars = cars.pop(2)
-print(f"{first_owend_cars.upper()} has been removed from the list")
