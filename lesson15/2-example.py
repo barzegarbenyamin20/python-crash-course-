76b2fd86bfd0b9bd0131b75ac99f09dfc5b442b2
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:02:25 +0330

    Delete lesson15/2-example.py

--- lesson15/2-example.py
+++
@@ -1,9 +0,0 @@
-favorite_language = {
-    'benyamin': 'JavaScript',
-    'taha': 'Python',
-    'mehrsam': 'C',
-    'hesam': 'Rust'
-}
-
-for name, lang in favorite_language.items():
-    print(f"{name.title()}'s favorite programming language is {lang.title()}")
