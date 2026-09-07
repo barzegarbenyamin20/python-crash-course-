6fd5e9cccee351c96a15d1f76e4fb26970f4c8d6
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:03:33 +0330

    Delete lesson16

--- lesson16/3-sorthed-method.py
+++
@@ -1,9 +0,0 @@
-favorite_language = {
-    'benyamin': 'JavaScript',
-    'taha': 'JavaScript',
-    'hesam': 'C',
-    'mehrsam': 'Rust'
-}
-print('following langs is => ')
-for lang in set(favorite_language.values()):
-    print(lang.title())
