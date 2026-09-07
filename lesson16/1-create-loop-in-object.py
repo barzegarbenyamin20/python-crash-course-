50d4a8d3eb8f23c4fa30620d53c9ff57c2eb05f2
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:03:03 +0330

    Delete lesson16/1-create-loop-in-object.py

--- lesson16/1-create-loop-in-object.py
+++
@@ -1,9 +0,0 @@
-favorite_language = {
-    'benyamin': 'JavaScript',
-    'taha': 'Python',
-    'hesam': 'C',
-    'mehrsam': 'Rust'
-}
-# sorted method
-for name in sorted(favorite_language.keys()):
-    print(f"{name.title()}Thank you for taking the poll")
