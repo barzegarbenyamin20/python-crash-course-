0b63c346d45796ba18770b8f61f34877cf2627f3
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:21:37 +0330

    Delete python crash course/lesson7/2-moreacrionswitchloopsinarray.py

--- python crash course/lesson7/2-moreacrionswitchloopsinarray.py
+++
@@ -1,6 +0,0 @@
-cars = ['bmw','audi','benz','renult']
-for i in cars:
-    print(f"{i.upper()}, what was a great list")
-    print(f"i cant wait for next cars of {i.upper()}\n")
-
-print("tank yuo everyone")
