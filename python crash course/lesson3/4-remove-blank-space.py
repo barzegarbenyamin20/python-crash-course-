3596ba741ed683525343fb6555e634eb8bf2d101
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:16:58 +0330

    Delete python crash course/lesson3/4-remove-blank-space.py

--- python crash course/lesson3/4-remove-blank-space.py
+++
@@ -1,8 +0,0 @@
-name = "benyamin        "
-print(name.rstrip())
-name2 = "   benyamin"
-print(name2.lstrip())
-name3 = "   benyamin    "
-print(name3.strip())
-withtab= "\thello world python"
-print(withtab.lstrip())
