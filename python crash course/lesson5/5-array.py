1144c6096589a59ce9284b59ac0930d24a8a4857
BENYAMIN BARZEGAR <barzegarbenyamin20@gmail.com>
2026-09-07 23:19:17 +0330

    Delete python crash course/lesson5/5-array.py

--- python crash course/lesson5/5-array.py
+++
@@ -1,9 +0,0 @@
-car = ["bmw","bugati","scania","volvo"]
-car.append('peykan')
-print(car)
-cars = []
-cars.append("perayd")
-cars.append("peykan")
-cars.append("tara")
-cars.append("samand")
-print(cars)
